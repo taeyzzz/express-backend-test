4d4757d55583c0933773ee6263014b4cd34e0d83
taeyzzz <tkul@sertiscorp.com>
2019-11-22 12:36:13 +0700

    fix validtor

--- src/validator/cards/index.js
+++ src/validator/cards/index.js
@@ -1,10 +1,10 @@
 const { check } = require('express-validator')
 
 const createCardValidationRule = [
-  check("name").exists().trim().isLength({ min: 1 }).withMessage("name is required."),
-  check("status").exists().trim().isLength({ min: 1 }).withMessage("status is required."),
-  check("content").exists().trim().isLength({ min: 1 }).withMessage("content is required."),
-  check("category").exists().trim().isLength({ min: 1 }).withMessage("category is required.")
+  check("name").trim().isLength({ min: 1 }).withMessage("name is required."),
+  check("status").trim().isLength({ min: 1 }).withMessage("status is required."),
+  check("content").trim().isLength({ min: 1 }).withMessage("content is required."),
+  check("category").trim().isLength({ min: 1 }).withMessage("category is required.")
 ]
 
 module.exports = {
--- src/validator/register/index.js
+++ src/validator/register/index.js
@@ -1,10 +1,10 @@
 const { check } = require('express-validator')
 
 const registerValidationRule = [
-  check("email").exists().trim().isEmail().withMessage("Email is not valid."),
-  check("password").exists().trim().isLength({ min: 6 }).withMessage("password is no valid."),
-  check("firstName").exists().trim().isLength({ min: 1 }).withMessage("firstName is required."),
-  check("lastName").exists().trim().isLength({ min: 1 }).withMessage("lastName is required.")
+  check("email").trim().isEmail().withMessage("Email is not valid."),
+  check("password").trim().isLength({ min: 6 }).withMessage("password is no valid."),
+  check("firstName").trim().isLength({ min: 1 }).withMessage("firstName is required."),
+  check("lastName").trim().isLength({ min: 1 }).withMessage("lastName is required.")
 ]
 
 module.exports = {
